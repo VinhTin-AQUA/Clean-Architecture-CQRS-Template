f9061c5ed799f0964cbb99573138ae63c9718f18
VinhTin-AQUA <tinhovinh@gmail.com>
2024-09-11 00:09:41 +0700

    add gitignore

--- CleanArchitectureWithCQRS/Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
+++ CleanArchitectureWithCQRS/Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+93115fe48c6a2045e0cfc99988f00749f7dce18e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
